24be325cec0e8791194e65acfd3d707a73b7e444
Steezzzy <99232091+Steezzzy@users.noreply.github.com>
2022-03-03 20:19:26 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,21 @@ Route::get('/board', function () {
     return view('board');
 });
 
+Route::get('/db-test',function () {
+        try {
+             \DB::connection()-> getPDO();
+             $db_name = \DB::connection()->getDatabaseName();
+             echo 'Datebase Connected: '.$db_name;
+        }catch (\Exception $e) {
+                echo 'None';
+        }
 
+});
+
+Route::get('/db-migrate', function() {
+            Artisan::cal('migrate');
+            echo Artisan::output();
+});
 
 
 Route::get('/events-feed', function(){
